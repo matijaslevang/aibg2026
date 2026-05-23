e3e96e6245fe56b5416eaa653feb437911f8b1ab
KingKryle <thekingkryle257@gmail.com>
2026-05-24 01:59:15 +0200

    further improvements

--- index.py
+++ index.py
@@ -101,7 +101,7 @@ def run(preset='balanced'):
 
                 action = decide(state, bot.player_id, turn=bot.turn_count,
                                 recent_positions=bot.recent_positions, preset=bot.preset,
-                                time_limit=20.0)
+                                time_limit=15.5)
 
                 me_raw   = state.get('Players', {}).get(str(bot.player_id), {})
                 statuses = me_raw.get('ActiveStatuses') or {}
